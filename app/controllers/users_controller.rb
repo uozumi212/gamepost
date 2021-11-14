2da33d52f82443e2afb285a69c86aafe426b3500
kazuma.uozumi <kazu3278mana@gmail.com>
2021-11-14 08:46:18 +0000

    gamepost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
