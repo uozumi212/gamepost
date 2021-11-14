310a8fbee3b86a2f488313b2ad1cfcfd9b9fadd8
kazuma.uozumi <kazu3278mana@gmail.com>
2021-11-14 12:43:39 +0000

    gamepost

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 
 Developer.create(content: "CAPCOM")
+Developer.create(content:  "任天堂")
+Developer.create(content:  "タカラトミー")
 Distributor.create(content: "KONAMI")
+Distributor.create(content:  "セガ")
+Distributor.create(content: "ブシロード" )
